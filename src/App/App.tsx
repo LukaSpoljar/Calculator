dec6f01dc7d43d96d60eb01ea0f51d5d7b7c04db
Ivan Luka Špoljar <lukispoljar@gmail.com>
2025-09-12 18:23:20 +0200

    new-update

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,7 +3,6 @@ import './App.scss';
 import Button from '@mui/material/Button';
 import { useEffect } from 'react';
 import BackspaceIcon from '@mui/icons-material/Backspace';
-import IconButton from '@mui/material/IconButton';
 
 export default function App() {
 
@@ -42,19 +41,40 @@ export default function App() {
             buttonsPad?.addEventListener('click', (event: any) => {
                 userInput?.focus();
                 (resultText as HTMLParagraphElement).innerHTML = `&nbsp;`;
-                if (event.target == clearButton) {
-                    if (userInput) {
-                        (userInput as HTMLInputElement).value = (userInput as HTMLInputElement).value.toString().slice(0, -1);;
-                    }
-                } else if (event.target == resultButton) {
+                if (event.target == resultButton) {
                     calculate();
-                } else if (event.target instanceof HTMLButtonElement) {
+                } else if (event.target instanceof HTMLButtonElement && event.target != clearButton) {
                     let buttonSymbol = event.target.textContent;
-
                     if (userInput)
                         (userInput as HTMLInputElement).value += buttonSymbol;
                 }
             });
+
+            //Pointer on CLEAR BUTTON (when deleting)
+
+            let intervalID = 0;
+            clearButton?.addEventListener('pointerdown', (event: any) => {
+
+                if (userInput) {
+                    let startTime = Date.now();
+                    let timeDifference = 0;
+
+                    const repeatFn = () => {
+                        timeDifference = Date.now() - startTime;
+
+                        if (timeDifference <= 1200) (userInput as HTMLInputElement).value = (userInput as HTMLInputElement).value.toString().slice(0, -1);
+                        else if (timeDifference <= 2000) (userInput as HTMLInputElement).value = (userInput as HTMLInputElement).value.toString().slice(0, -2);
+                        else {
+                            let stringValue = (userInput as HTMLInputElement).value.toString();
+                            (userInput as HTMLInputElement).value = stringValue.slice(0, -stringValue.length);
+                        }
+                    }
+
+                    intervalID = window.setInterval(repeatFn, 50);
+                }
+            });
+            clearButton?.addEventListener('pointerup', () => clearInterval(intervalID));
+            clearButton?.addEventListener('pointercancel', () => clearInterval(intervalID));
         }
     }, [])
 
@@ -71,10 +91,10 @@ export default function App() {
             </div>
             <div id='down-section-wrapper'>
                 <div id='side-operations'>
-                    <Button variant='contained' size="large">&divide;</Button>
                     <Button variant='contained' size="large">&times;</Button>
-                    <Button variant='contained' size="large">&minus;</Button>
                     <Button variant='contained' size="large">&#43;</Button>
+                    <Button variant='contained' size="large">&minus;</Button>
+                    <Button variant='contained' size="large">&divide;</Button>
                 </div>
                 <div id='symbols-and-digits-wrapper'>
                     <div className='one-row'>
@@ -107,7 +127,6 @@ export default function App() {
                         <Button id='result_button' variant='contained' size="large">=</Button>
                     </div>
                 </div>
-
             </div>
         </div>
     )
